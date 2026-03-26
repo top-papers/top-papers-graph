f5e609bfa8ff04dfcc8eb1fb9254a5d51a7fddee
visual comments <49592231+visualcomments@users.noreply.github.com>
2026-03-26 16:25:21 +0300

    repair

--- src/scireason/net/__init__.py
+++
@@ -1,2 +0,0 @@
-from .http import get_json, get_text, default_client
-from .http_client import HttpClient, HttpClientConfig
--- src/scireason/papers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .schema import PaperMetadata, PaperSource, Author, ExternalIds, Venue
-from .service import search_papers, get_paper_by_doi
-from .resolver import resolve_ids, ResolvedIds
